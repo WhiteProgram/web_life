5c29dfe4f650a36dc9689668cf282750a1907fc1
mingjie <1782370925@qq.com>
2019-05-17 22:37:36 +0800

    1.第一个版本完成

---
+++ life_server/src/main/java/com/mingjie/life_server/LifeServerApplication.java
@@ -0,0 +1,13 @@
+package com.mingjie.life_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeServerApplication.class, args);
+    }
+
+}
---
+++ life_server/src/test/java/com/mingjie/life_server/LifeServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mingjie.life_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LifeServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
